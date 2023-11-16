b466d42c8ae5fccb8667400636340a7cef4c57db
Gonzalez Joaquin <joa.contacto.gz@gmail.com>
2023-11-15 23:24:34 -0300

    V - 0.0.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
+const url = 'http://localhost:5000/'
+
 document.getElementById('loginForm').addEventListener('submit', event => {
     event.preventDefault()
 
-    const email = document.querySelector('#loginForm [name = "user_name_login"]').value;
+    const username = document.querySelector('#loginForm [name = "user_name_login"]').value;
     const password = document.querySelector('#loginForm [name = "password_login"]').value;
 
-    if (email.value === '' !== password.value === '') {
+    if (username.value === '' !== password.value === '') {
         alert('Complete los campos para continuar')
         return
     }
 
-    loginWithEmailAndPassword(email, password)
+    loginWithUsernameAndPassword(username, password)
 
     document.querySelector('#loginForm [name = "user_name_login"]').value = '';
     document.querySelector('#loginForm [name = "password_login"]').value = '';
 })
 
-const loginWithEmailAndPassword = async (email, password) => {
-    
+const loginWithUsernameAndPassword = async (username, password) => {
+    const userData = { username, password }
+    const options = { method: 'POST', body: JSON.stringify(userData) }
+
+    fetch(`${url}users/login`, options)
+        .then(response => {
+            if (response.status === 200) {
+                window.location.href = 'home.html'
+            }
+        })
+        .catch(err => {
+            throw new Error('Surgió un error: ', err)
+        })
 }
